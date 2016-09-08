648c91b8cd1874b27277ddfa6f83c56b038045a9
孙佳琪 <singular7310@126.com>
2016-09-08 15:08:06 +0800

    second

---
+++ Pocoo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pocoo
+//
+//  Created by singular on 16/9/8.
+//  Copyright © 2016年 孙佳琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, "7.0"
+
+inhibit_all_warnings!
+
+
+target 'Pocoo' do
+
+
+pod 'Masonry'
+pod 'SDWebImage', '~>3.7'
+pod "AFNetworking", "~> 2.0"
+pod 'MBProgressHUD', '~> 0.9.1'
+pod 'IQKeyboardManager'
+pod 'MJRefresh'    , '~>2.2.1'
+pod 'MJExtension'
+
+end
